d7eb20813de69574b2643fb3130d2e36098acc94
Vita <vitakuznetsovav@gmail.com>
2023-10-12 19:42:12 +0300

    header links

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,20 @@
 import { FC } from 'react';
+import { Routes, Route } from "react-router-dom";
 import { Header } from './features/Header';
 import { Footer } from './features/Footer';
+import { Home } from './pages/home/Home';
 
 export const App: FC = () => {
   return (
     <>
       <Header />
-      {/* router */}
+
+      <main>
+        <Routes>
+          <Route path='/' element={<Home />} />
+        </Routes>
+      </main>
+
       <Footer />
     </>
   );
